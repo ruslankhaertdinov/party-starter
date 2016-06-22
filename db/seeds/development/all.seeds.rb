c8abd53a61e869ac16b67ea02425caa2717f5f83
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2016-06-22 17:23:43 +0300

    rename date_time fields with <at> suffix

--- db/seeds/development/all.seeds.rb
+++ db/seeds/development/all.seeds.rb
@@ -18,15 +18,15 @@ FactoryGirl.create(:invitation, user: user_2, event: event)
 FactoryGirl.create(:invitation, user: user_3, event: event)
 
 puts "Creating availabilities..."
-FactoryGirl.create(:availability, user: user_1, start_time: Time.zone.parse("Fri, 17 Jun 2016 12:00:00"), end_time: Time.zone.parse("Fri, 17 Jun 2016 13:00:00"))
-FactoryGirl.create(:availability, user: user_1, start_time: Time.zone.parse("Fri, 17 Jun 2016 14:00:00"), end_time: Time.zone.parse("Fri, 17 Jun 2016 15:00:00"))
-FactoryGirl.create(:availability, user: user_1, start_time: Time.zone.parse("Fri, 17 Jun 2016 16:00:00"), end_time: Time.zone.parse("Fri, 17 Jun 2016 17:00:00"))
+FactoryGirl.create(:availability, user: user_1, start_at: Time.zone.parse("Fri, 17 Jun 2016 12:00:00"), end_at: Time.zone.parse("Fri, 17 Jun 2016 13:00:00"))
+FactoryGirl.create(:availability, user: user_1, start_at: Time.zone.parse("Fri, 17 Jun 2016 14:00:00"), end_at: Time.zone.parse("Fri, 17 Jun 2016 15:00:00"))
+FactoryGirl.create(:availability, user: user_1, start_at: Time.zone.parse("Fri, 17 Jun 2016 16:00:00"), end_at: Time.zone.parse("Fri, 17 Jun 2016 17:00:00"))
 
-FactoryGirl.create(:availability, user: user_2, start_time: Time.zone.parse("Fri, 17 Jun 2016 14:00:00"), end_time: Time.zone.parse("Fri, 17 Jun 2016 15:00:00"))
-FactoryGirl.create(:availability, user: user_2, start_time: Time.zone.parse("Fri, 17 Jun 2016 16:00:00"), end_time: Time.zone.parse("Fri, 17 Jun 2016 17:00:00"))
-FactoryGirl.create(:availability, user: user_2, start_time: Time.zone.parse("Fri, 17 Jun 2016 18:00:00"), end_time: Time.zone.parse("Fri, 17 Jun 2016 19:00:00"))
+FactoryGirl.create(:availability, user: user_2, start_at: Time.zone.parse("Fri, 17 Jun 2016 14:00:00"), end_at: Time.zone.parse("Fri, 17 Jun 2016 15:00:00"))
+FactoryGirl.create(:availability, user: user_2, start_at: Time.zone.parse("Fri, 17 Jun 2016 16:00:00"), end_at: Time.zone.parse("Fri, 17 Jun 2016 17:00:00"))
+FactoryGirl.create(:availability, user: user_2, start_at: Time.zone.parse("Fri, 17 Jun 2016 18:00:00"), end_at: Time.zone.parse("Fri, 17 Jun 2016 19:00:00"))
 
-FactoryGirl.create(:availability, user: user_3, start_time: Time.zone.parse("Fri, 17 Jun 2016 16:00:00"), end_time: Time.zone.parse("Fri, 17 Jun 2016 17:00:00"))
-FactoryGirl.create(:availability, user: user_3, start_time: Time.zone.parse("Fri, 17 Jun 2016 18:00:00"), end_time: Time.zone.parse("Fri, 17 Jun 2016 19:00:00"))
-FactoryGirl.create(:availability, user: user_3, start_time: Time.zone.parse("Fri, 17 Jun 2016 20:00:00"), end_time: Time.zone.parse("Fri, 17 Jun 2016 21:00:00"))
+FactoryGirl.create(:availability, user: user_3, start_at: Time.zone.parse("Fri, 17 Jun 2016 16:00:00"), end_at: Time.zone.parse("Fri, 17 Jun 2016 17:00:00"))
+FactoryGirl.create(:availability, user: user_3, start_at: Time.zone.parse("Fri, 17 Jun 2016 18:00:00"), end_at: Time.zone.parse("Fri, 17 Jun 2016 19:00:00"))
+FactoryGirl.create(:availability, user: user_3, start_at: Time.zone.parse("Fri, 17 Jun 2016 20:00:00"), end_at: Time.zone.parse("Fri, 17 Jun 2016 21:00:00"))
 puts "All done."
