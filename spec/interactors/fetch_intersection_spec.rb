d7a667d80becab091e9cfce5e441381f4cf742f0
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2016-06-24 15:37:05 +0300

    update docs

--- spec/interactors/fetch_intersection_spec.rb
+++ spec/interactors/fetch_intersection_spec.rb
@@ -1,31 +1,69 @@
 require "rails_helper"
 
 describe FetchIntersection do
-  let(:event) { create(:event, owner: user_1) }
-  let(:user_1) { create(:user) }
-  let(:user_2) { create(:user) }
-  let(:user_3) { create(:user) }
+  let(:event) { create(:event) }
+
+  let(:user_1) do
+    create(:user, availability: {
+      monday: [
+        { start_at: 0, end_at: 24 }
+      ],
+      tuesday: [
+        { start_at: 0, end_at: 3 },
+        { start_at: 7, end_at: 24 }
+      ]
+    })
+  end
+
+  let(:user_2) do
+    create(:user, availability: {
+      monday: [
+        { start_at: 3, end_at: 12 },
+        { start_at: 14, end_at: 16 },
+        { start_at: 20, end_at: 24 }
+      ],
+      tuesday: [
+        { start_at: 1, end_at: 10 }
+      ]
+    })
+  end
+
+  let(:user_3) do
+    create(:user, availability: {
+      monday: [
+        { start_at: 5, end_at: 15 },
+        { start_at: 20, end_at: 22 }
+      ],
+      tuesday: [
+        { start_at: 0, end_at: 2 },
+        { start_at: 3, end_at: 12 }
+      ]
+    })
+  end
   let(:service) { described_class.new(event.users) }
 
-  context "when 1 to 1 availability matching" do
-    let(:expected_intersections) do
-      [
-        Time.zone.parse("Fri, 17 Jun 2016 12:20:00")..Time.zone.parse("Fri, 17 Jun 2016 13:00:00"),
-        Time.zone.parse("Fri, 17 Jun 2016 14:20:00")..Time.zone.parse("Fri, 17 Jun 2016 15:00:00")
+  let(:expected_intersections) do
+    {
+      monday: [
+        { start_at: 5, end_at: 12 },
+        { start_at: 14, end_at: 15 },
+        { start_at: 20, end_at: 22 }
+      ],
+      tuesday: [
+        { start_at: 1, end_at: 2 },
+        { start_at: 7, end_at: 10 }
       ]
-    end
+    }
+  end
 
-    before do
-      event.users << user_1
-      event.users << user_2
-      event.users << user_3
-    end
+  before do
+    event.users << user_1
+    event.users << user_2
+    event.users << user_3
+  end
 
 
-    it "returns intersections" do
-      pending
-
-      expect(service.call).to eq(expected_intersections)
-    end
+  it "returns intersections" do
+    expect(service.call).to eq(expected_intersections)
   end
 end
