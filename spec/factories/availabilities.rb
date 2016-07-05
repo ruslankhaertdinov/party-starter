dd3fc1c42e51c16c5778e60547e155fe95c278d3
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2016-07-05 22:57:05 +0300

    update docs

--- spec/factories/availabilities.rb
+++ spec/factories/availabilities.rb
@@ -3,4 +3,8 @@ FactoryGirl.define do
     event
     user
   end
+
+  trait :with_intervals do
+    intervals {{ tuesday: [{ start_at: "9", end_at: "10" }, { start_at: "18", end_at: "19" }] }}
+  end
 end
